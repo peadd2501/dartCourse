46824b27108d18922b1279526d81631f7cd3b2fb
Pelsar Add <peadd2501@gmail.com>
2023-08-16 18:12:35 -0600

    Se añadieron las clases faltantes

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,13 @@
+void main(){
+  
+  // var myName = 'Pelsar Donis';
+  // late final myName = 'Pelsar Donis';
+  // String myName = 'Pelsar Donis';
+
+  const myName = 'Pelsar Donis';
+  
+  print('Mi nombre es: $myName');
+  print('Mi nombre es: ${myName.toLowerCase()}');
+
+}
+
